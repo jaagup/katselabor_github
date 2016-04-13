9178db073d10d3f5cf46a6664057eabacafbe42d
jaagup <jaagup@tlu.ee>
2016-04-13 13:47:29 +0300

    Arvude Generaatori uus projekt

---
+++ Generaatorid/YhtlaseJaotusegaGeneraator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generaatorid
+{
+    class YhtlaseJaotusegaGeneraator : IArvudeGeneraator
+    {
+        public double[] looArvud(int kogus, double param1, double param2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
