8e8b71dfe7ff0d3d0cb77097c062a6845d02aa82
Hardo Kampus <hardok@tlu.ee>
2016-04-19 14:25:24 +0300

    minu kodune motlemine

---
+++ Database1/dbo/Tables/students.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL  PRIMARY KEY IDENTITY , 
+    [name] VARCHAR(50) NULL, 
+    [length] NCHAR(10) NULL, 
+    [mass] DECIMAL NULL, 
+    [sex] NCHAR(10) NULL, 
+    [entryDate] DATETIME NULL
+)
