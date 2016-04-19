b3225aa3a0778ceb89301b1cd2834838cc2cf9ca
Sander Soots <kameeleonlik@gmail.com>
2016-04-19 14:13:02 +0300

    fixed db. guid cannot be autoincement. silly me.

--- Database1/dbo/Tables/generationHistorySet.sql
+++ Database1/dbo/Tables/generationHistorySet.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[generationHistorySet] (
-    [Id]     UNIQUEIDENTIFIER NOT NULL IDENTITY,
+    [Id]     UNIQUEIDENTIFIER NOT NULL,
     [result] NVARCHAR (MAX)   NOT NULL,
     CONSTRAINT [PK_generationHistorySet] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
