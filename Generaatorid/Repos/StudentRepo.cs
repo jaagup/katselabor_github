2903ba71be641d73d9ffefdb03863099e251bb09
Hardo Kampus <hardok@tlu.ee>
2016-04-19 22:21:03 +0300

    Fixed search bugs, Returns many, possible to edit in search view.

--- Generaatorid/Repos/StudentRepo.cs
+++ Generaatorid/Repos/StudentRepo.cs
@@ -45,15 +45,15 @@ namespace katselabor.Repos
             using (var ctx = new katselaborEntities())
             {
                 var list = new List<students>();
-
                 var L2EQuery = from st in ctx.students
-                               where st.name == searchQ
-                               select st;
-
-                //tagastab ainult esimese vastuse praegu
-                var student = L2EQuery.FirstOrDefault<students>();
-                list.Add(student);
-
+                                where st.name.Contains(searchQ)
+                                select st ;
+                //tagastab kõik %LIKE% searchQ
+                var student = L2EQuery.ToArray<students>();
+                for(int i = 0; i< student.Length; i++)
+                {
+                    list.Add(student[i]);
+                }
                 // tagastab listi
                 logger.Info("Hopefully returning more than 1 student matching");
                 return list.ToArray();
